8c5bc956e7a7956e565307a219a04dfd4b86ee2f
TeckLeong <teckleong@-NWXW43QXK2-3.local>
2024-07-24 13:26:50 +0800

    user1

--- manifests/site.pp
+++ manifests/site.pp
@@ -19,6 +19,10 @@ node 'sgdemope.atl88.online' {
 
 node 'sgdemorocky1.atl88.online' {
   include sce_linux
+
+  user { 'user1':
+    ensure => 'present',
+  }
 }
 
 node 'sgdemowin1.atl88.online' {
