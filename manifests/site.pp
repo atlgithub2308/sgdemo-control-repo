5a49fc8f9ce2ccbb0568d0002beda53892f3f197
TeckLeong <teckleong@-NWXW43QXK2-3.local>
2024-10-09 12:43:48 +0800

    aqqa

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,7 +33,7 @@ node 'sgdemorocky1.atl88.online' {
 }
 
 node 'sgdemorocky2.atl88.online' {
-  include role::sample_website
+  #include role::sample_website
 }
 
 node 'sgdemodebian1.atl88.online' {
