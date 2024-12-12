fa205dc977768858f1857ac657fe475ff9c5a898
TeckLeong <teckleong@-NWXW43QXK2-3.local>
2024-12-12 17:17:48 +0800

    chnages to the content of my web site

--- manifests/site.pp
+++ manifests/site.pp
@@ -147,7 +147,7 @@ node 'sgdemorocky2.atl88.online' {
                     </style>
                   </head>
                   <body>
-                    <h1>Welcome Bharat Petroleum to Puppet World!</h1>
+                    <h1>Welcome Bharat Petroleum. My company is awesome!</h1>
                     <div class='card'>
                       <p>Congratulations! Your web server is up and running smoothly.</p>
                       <p>This server is powered by <strong>Apache</strong> on <strong>Rocky Linux</strong>.</p>
