5a0f6380fc2c5b7a13cd5cd6fe544a7615009b39
TeckLeong <teckleong.ang@perforce.com>
2025-11-26 12:42:47 +0800

    already declared

--- manifests/site.pp
+++ manifests/site.pp
@@ -83,7 +83,7 @@ node 'sgdemowin1.atl88.online' {
 }
 
 node 'sgdemowin2.atl88.online' {
-  include sce_windows
+  #include sce_windows
   include profile::mssql_install
   include profile::mssql_dsc
 
